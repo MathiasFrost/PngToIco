4dbb72da9731eb6626ea89c865a1745453277c4e
Mathias Løken <lakavi@mailfence.com>
2024-01-09 17:06:24 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("No file path provided.");
+    Console.WriteLine("Usage: [ProgramName] [PathToPNGFile]");
+    return;
+}
+
+string filePath = args[0];
+
+// Check if the file exists
+if (!File.Exists(filePath))
+{
+    Console.WriteLine($"The file '{filePath}' does not exist.");
+    return;
+}
+
+// Check if the file is a PNG file
+if (Path.GetExtension(filePath).ToLower() != ".png")
+{
+    Console.WriteLine("The file is not a PNG file. Do you still want to attempt conversion? (Y):");
+    int res = Console.Read();
+    if (Char.ToLower(Convert.ToChar(res)) != 'y') return;
+}
+
+// Determine the output file path
+string outputFilePath = args.Length > 1 ? args[1] : Path.ChangeExtension(filePath, ".ico");
+
+// Check if the output path is a directory
+if (Directory.Exists(outputFilePath))
+{
+    string fileName = Path.GetFileNameWithoutExtension(filePath) + ".ico";
+    outputFilePath = Path.Combine(outputFilePath, fileName);
+}
+
+try
+{
+    using FileStream stream = File.OpenRead(filePath);
+    var image = new Bitmap(stream);
+
+    image.Save(outputFilePath, ImageFormat.Icon);
+    Console.WriteLine($"The ICO file has been saved as: {outputFilePath}");
+}
+catch (Exception e)
+{
+    Console.WriteLine("An error occurred: " + e.Message);
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# PngToIco - _.NET_
+
+Extremely simple command line tool to convert .png files to .ico files using `System.Drawing.Bitmap`  
+Usage:
+
+```shell
+pngtoico path/to/png.png
+```
+
+Optionally with output path:
+
+```shell
+pngtoico path/to/png.png path/to/output.ico
+```
